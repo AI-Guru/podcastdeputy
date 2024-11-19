c418e1bbe87d701c0a6102cd80220c844d9aec71
Tristan Behrens <tristan@ai-guru.de>
2024-11-19 13:21:27 +0100

    First stable implementation.

--- app.py
+++ app.py
@@ -6,10 +6,13 @@ import time
 import logging
 import uuid
 import dotenv
+import langchain_core
 from langchain_core.prompts.chat import HumanMessagePromptTemplate, AIMessagePromptTemplate, SystemMessagePromptTemplate
 from langchain_community.chat_models import ChatOllama
+from langchain_anthropic import ChatAnthropic
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.document_loaders import WebBaseLoader
 from source.texttospeech import ElevenLabsTextToSpeech
 
 
@@ -17,9 +20,19 @@ from source.texttospeech import ElevenLabsTextToSpeech
 default_canvas_text = open("assets/defaulttext.txt", "r").read()
 default_canvas_text = "Herzlich willkommen zum AI Update."
 
+default_sources_text = open("assets/defaultsources.txt", "r").read()
+
 # Load the environment variables.
 dotenv.load_dotenv()
 
+text_to_speech = ElevenLabsTextToSpeech(
+    api_key=os.getenv("ELEVENLABS_API_KEY"),
+    voice_id="9gSkuKCHRczfU5aLq1qU"
+    #voice_id="yCJwUkOEJeSHB8xTh6HQ", #Robert?
+    #voice_clone_name="Robert",
+    #voice_clone_description="Ein 46-jähriger deutscher Mann!",
+    #voice_clone_samples=["assets/voice_sample_robert.mp3"]
+)
 
 class Application:
 
@@ -29,6 +42,11 @@ class Application:
         self.logger.setLevel(logging.DEBUG)
         self.logger.addHandler(logging.StreamHandler())
         self.logger.info("Starting Gradio Application")
+
+        self.chat_messages = []
+        self.podcast_text = ""
+
+        self.processed_sources = []
 
         #self.text_to_speech = ElevenLabsTextToSpeech(
         #    api_key=os.getenv("ELEVENLABS_API_KEY"),
@@ -51,10 +69,10 @@ class Application:
                     self.chatbot = ChatOllama()
 
                     # Create the chatbot's text field.
-                    self.chatbot_text_field = gr.Textbox(lines=5, label="Chatbot", placeholder="Chatbot's response")
+                    self.chatbot_element = gr.Chatbot(label="Chatbot", value=self.chat_messages, type="messages")
 
                     # Create the user's text field.
-                    self.user_text_field = gr.Textbox(lines=5, label="User", placeholder="Type here")
+                    self.sources_element = gr.Textbox(lines=5, label="Sources", placeholder="Type here", value=default_sources_text)
 
                     # Create the button to send the user's message.
                     self.send_button = gr.Button("Send")
@@ -77,14 +95,14 @@ class Application:
 
             # Create the event handlers.
             self.send_button.click(
-                self.process_user_message,
-                inputs=[self.user_text_field],
-                outputs=[self.chatbot_text_field, self.canvas_field]
+                self.process_sources,
+                inputs=[self.sources_element],
+                outputs=[self.chatbot_element, self.canvas_field]
             )
-            self.user_text_field.submit(
-                self.process_user_message,
-                inputs=[self.user_text_field],
-                outputs=[self.chatbot_text_field, self.canvas_field]
+            self.sources_element.submit(
+                self.process_sources,
+                inputs=[self.sources_element],
+                outputs=[self.chatbot_element, self.canvas_field]
             )
             self.speech_button.click(
                 self.text_to_speech,
@@ -96,15 +114,109 @@ class Application:
                 inputs=[self.canvas_field]
             )
 
-    def process_user_message(self, user_text):
-        return
+    def process_sources(self, sources):
+
+        def compile_yield():
+            return self.chat_messages, self.podcast_text
+
+        urls = sources.split("\n")
+        urls = [url.strip() for url in urls if url.strip() != ""]
+        
+        # See if all is okay.
+        for url in urls:
+            if not url.startswith("http"):
+                self.add_chat_message("assistant", f"Invalid URL: {url}")
+                yield compile_yield()
+                raise StopIteration()
+        
+        # Process the sources.
+        for url in urls:
+
+            # If it already has been processed, skip.
+            if any([processed_source["url"] == url for processed_source in self.processed_sources]):
+                self.add_chat_message("assistant", f"URL already processed: {url}")
+                yield compile_yield()
+                continue
+
+            self.add_chat_message("assistant", f"Processing URL: {url}")
+            yield compile_yield()
+
+            # Skip if the URL is already processed.
+            if any([processed_source["url"] == url for processed_source in self.processed_sources]):
+                self.add_chat_message("assistant", f"URL already processed: {url}")
+                yield compile_yield()
+                continue
+
+            # Load the document and get the page content.
+            loader = WebBaseLoader(url)
+            documents = loader.load()
+            assert len(documents) == 1, f"Expected 1 document, but got {len(documents)}"
+            document = documents[0]
+            page_content = document.page_content
+
+            # Process the content.
+            system_message = SystemMessagePromptTemplate.from_template_file("prompttemplates/system.txt", input_variables=[])
+            system_message = system_message.format()
+            human_message = HumanMessagePromptTemplate.from_template_file("prompttemplates/summarizetask.txt", input_variables=[])
+            human_message = human_message.format(
+                content=page_content,
+            )
+            processed_content = self.invoke_model(system_message, human_message)
+
+            # Add the processed content to the chat.
+            self.add_chat_message("assistant", f"Processed content: {processed_content}")
+            yield compile_yield()
+
+            # Save the processed source.
+            processed_source = {
+                "url": url,
+                "original_text": page_content,
+                "processed_text": processed_content,
+            }
+            self.processed_sources.append(processed_source)
+
+            # Count the number of words.
+            words = page_content.split(" ")
+            self.add_chat_message("assistant", f"Number of words roughly: {len(words)}")
+
+            yield compile_yield()
+
+
+        # Update the chat.
+        self.add_chat_message("assistant", f"Done processing sources. Now processing the sources.")
+        yield compile_yield()
+
+        # Now process the sources.
+        contents = "Here are the processed sources:\n\n"
+        for processed_source_index, processed_source in enumerate(self.processed_sources):
+            contents += f"Source {processed_source_index + 1}:\n"
+            contents += "```\n"
+            contents += processed_source["processed_text"]
+            contents += "\n```\n\n" 
+
+        # Get the current date. Format is DD.MM.YYYY.
+        current_date = time.strftime("%d.%m.%Y")
+
+        # Language is German.
+        language = "German"
+
+        # Process the document.
+        system_message = SystemMessagePromptTemplate.from_template_file("prompttemplates/system.txt", input_variables=[])
+        system_message = system_message.format()
+        human_message = HumanMessagePromptTemplate.from_template_file("prompttemplates/rewritetask.txt", input_variables=[])
+        human_message = human_message.format(
+            contents=contents,
+            current_date=current_date,
+            language=language
+        )
+        self.podcast_text = self.invoke_model(system_message, human_message)
+
+        self.add_chat_message("assistant", f"Done processing sources.")
+        yield compile_yield()
+
     
     def text_to_speech(self, text):
-        text_to_speech = ElevenLabsTextToSpeech(
-            api_key=os.getenv("ELEVENLABS_API_KEY"),
-            voice_id="9gSkuKCHRczfU5aLq1qU"
-            #voice_id="yCJwUkOEJeSHB8xTh6HQ"
-        )
+        
         audio = text_to_speech.to_speech(text)
 
         # Save the audio to a file.
@@ -118,6 +230,55 @@ class Application:
     
     def download_text(self, text):
         return
+    
+    def sources_to_text(self):
+
+        text = ""
+
+        for processed_source in self.processed_sources:
+            url = processed_source["url"]
+            original_text = processed_source["original_text"]
+            processed_text = processed_source["processed_text"]
+
+            text += processed_text + "\n\n"
+
+        return text
+
+
+    def add_chat_message(self, role, content, is_article_message=False):
+        assert role in ["user", "assistant"], f"Invalid role: {role}"
+        new_message = {"role": role, "content": content}
+        self.chat_messages.append(new_message)
+        if is_article_message:
+            self.article_messages.append(new_message)
+
+
+    def invoke_model(self, system_message, human_message):
+        assert isinstance(system_message, langchain_core.messages.system.SystemMessage), f"Expected a string for the system message, but got {type(system_message)}"
+        assert isinstance(human_message, langchain_core.messages.human.HumanMessage), f"Expected a string for the human message, but got {type(human_message)}"
+        self.logger.info(f"System message: {system_message}")
+        self.logger.info(f"Human message: {human_message}")
+
+        return self.invoke_model_messags([system_message, human_message])
+
+
+    def invoke_model_messags(self, messages):
+        assert isinstance(messages, list), f"Expected a list of messages, but got {type(messages)}"
+        llm = self.get_llm()
+        prompt = ChatPromptTemplate.from_messages(messages)
+        chain = prompt | llm | StrOutputParser()
+        text = chain.invoke({})
+        self.logger.info(f"Model response: {text}")
+        return text
+    
+
+    def get_llm(self):
+        return ChatAnthropic(
+            model="claude-3-5-sonnet-20241022",
+            api_key=os.getenv("ANTHROPIC_API_KEY"),
+            temperature=0.7,
+            max_tokens=8192,
+        )
     
 
 
--- source/texttospeech.py
+++ source/texttospeech.py
@@ -1,3 +1,4 @@
+import os
 from typing import Iterator
 import numpy as np
 from elevenlabs.client import ElevenLabs
@@ -7,15 +8,46 @@ class ElevenLabsTextToSpeech:
 
     def __init__(
             self, 
-            api_key,
-            voice_id,
-            model_id="eleven_multilingual_v2",
-            langauge_code="de_DE"
+            api_key:str,
+            voice_id:str = None,
+            voice_clone_name:str = None,
+            voice_clone_description:str = None,
+            voice_clone_samples:list[str] = None, 
+            model_id:str = "eleven_multilingual_v2",
+            langauge_code:str = "de_DE"
         ):
+
+        has_voice_id = voice_id is not None
+        has_voice_clone = any([voice_clone_name is not None, voice_clone_description is not None, voice_clone_samples is not None])
+        if not has_voice_id and not has_voice_clone:
+            raise ValueError("Either voice_id or voice_clone_name must be provided.")
+        if has_voice_id and has_voice_clone:
+            raise ValueError("Either voice_id or voice_clone_name must be provided, not both.")
+        if has_voice_clone:
+            if not all([voice_clone_name, voice_clone_description, voice_clone_samples]):
+                raise ValueError("voice_clone_name, voice_clone_description, and voice_clone_samples must be provided.")
+
         self.api_key = api_key
-        self.voice_id = voice_id
         self.model_id = model_id
         self.language_code = langauge_code
+
+        # Create the client.
+        self.client = ElevenLabs(
+            api_key=self.api_key
+        )
+
+        # Handle the voices.
+        if voice_id is not None:
+            self.voice=Voice(
+                voice_id=voice_id,
+                settings=VoiceSettings(stability=0.71, similarity_boost=0.5, style=0.0, use_speaker_boost=True)
+            )
+        elif voice_clone_samples is not None and voice_clone_name is not None:
+            self.voice = self.client.clone(
+                name=voice_clone_name,
+                description=voice_clone_description,
+                files=voice_clone_samples,
+            )
 
 
     def to_speech_requests(self, text):
@@ -67,16 +99,11 @@ class ElevenLabsTextToSpeech:
 
 
     def to_speech(self, text):
-        client = ElevenLabs(
-            api_key=self.api_key
-        )
-        voice=Voice(
-            voice_id=self.voice_id,
-            settings=VoiceSettings(stability=0.71, similarity_boost=0.5, style=0.0, use_speaker_boost=True)
-        )
-        audio = client.generate(
+
+
+        audio = self.client.generate(
             text=text, 
-            voice=voice,
+            voice=self.voice,
             model=self.model_id
         )
         if isinstance(audio, Iterator):
